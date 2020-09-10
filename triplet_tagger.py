97398eb89cca3f5fa79c9d1d30974ee1ca0c2aa0
Bobby Schiller <bobbyschiller99@gmail.com>
2020-09-09 20:28:16 -0400

    add triplet_tagger.py

---
+++ triplet_tagger.py
@@ -0,0 +1,12 @@
+## Class to define final connected layer
+# Author: Bobby Schiller
+# Last Modified: 9 September 2020
+
+import numpy as np
+import lorentzNN.lorentzNN as lNN
+import torch
+
+class triplet_tagger():
+
+  def __init(torch.nn.Module):
+    super
