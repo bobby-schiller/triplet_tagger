a0e2129819f64ab89db404b3d5f420ee5b2d6cda
Bobby Schiller <53233475+bobby-schiller@users.noreply.github.com>
2020-09-09 22:37:20 -0400

    fully define triplet_tagger class

--- triplet_tagger.py
+++ triplet_tagger.py
@@ -1,12 +1,20 @@
-## Class to define final connected layer
+## Class to define final, fully-connected layer
+# [input] : [40,1] - softmax outputs of all 20 lorentzNN triplets
+# [output] : [21,1] - softmax output tagging correct triplet (or none)
 # Author: Bobby Schiller
 # Last Modified: 9 September 2020
 
 import numpy as np
-import lorentzNN.lorentzNN as lNN
 import torch
 
-class triplet_tagger():
-
-  def __init(torch.nn.Module):
-    super
+class triplet_tagger(torch.nn.Module):
+  
+  def __init__(self, **kwargs):
+    super(triplet_tagger, self).__init__()
+    self.layer = torch.nn.Linear(40,21)
+    self.activation = torch.nn.Softmax(dim=1)
+  
+  def forward(self, x, **kwargs):
+    x = self.layer(x)
+    x = self.activation(x)
+    return x
