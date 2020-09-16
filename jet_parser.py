de9a83f4d697c96f0333fae443c0458b20372765
Bobby Schiller <53233475+bobby-schiller@users.noreply.github.com>
2020-09-16 17:23:27 -0400

    add parser to create correct target arrays

---
+++ jet_parser.py
@@ -0,0 +1,43 @@
+## Parser to convert jets.npz file into correct input files
+# Author: Bobby Schiller
+# Last Modified: 16 September 2020
+
+import numpy as np
+from numpy import load
+from itertools import combinations
+
+data = load('jets.npz')
+
+num_ev = len(data['match_validation'])
+
+# Generate combinations of triplets
+comb = list(combinations(range(6),3))
+
+# categorical and numerical forms
+match_train_cat = np.zeros(num_ev,21)
+match_train = np.zeros(num_ev,1]
+
+# discard events with more than 3 matches (not triplets)
+drop_event = []
+
+# compute match for each triplet; append to new match_
+for ev_i, event in enumerate(data['match_validation']):
+  if np.sum(event) > 3:
+    drop_event.append(ev_i)
+    continue
+  elif np.sum(event) == 0:
+    match_train_cat[ev_i][20] = 1
+    match_train[ev_i] = 20
+    continue
+  for iter_i, iter in enumerate(comb):
+    if np.sum(np.take(event,iter,0)) == 3:
+      match_train_cat[ev_i][iter_i] = 1
+      match_train[ev_i] = iter_i
+      continue
+
+target_cat = np.delete(match_train_cat,drop_event,0)
+target = np.delete(match_train,drop_event,0)
+input = np.delete(jetv_train,drop_event,0)
+
+np.savez("new_jt_validationFull_cat",target=target_cat,input=input)
+np.savez("new_jt_validationFull",target=target,input=input)
