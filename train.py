9e474d6a56622bb142bb9c88ca779ddbba860497
Bobby Schiller <53233475+bobby-schiller@users.noreply.github.com>
2020-09-17 01:27:39 -0400

    add LorentzNN connection

--- train.py
+++ train.py
@@ -16,41 +16,69 @@ model_file = 'out_file_e_20.tar'
 
 # generate list of combinations of 3 jets in [0,5]
 # itertools provides standard indexing for the triplet combinations 
-comb = combination(range(6),3)
+comb = list(combination(range(6),3))
 
 learning_rate = 0.001
+
+data = load('jets.npz')
+jetv_train = data['jetv_train']
 
 def train():
   
   # initialize LorentzNN model
   lorentz_model = lNN.LorentzNN("/scratch365/rschill1/nn/inputs/ \
                       lNN_standards/p_standard1.npz",
-                      i_shape=input_shape,
-                      device=args.device).to(device=args.device)
+                      i_shape=(200,4,3))
   
   # Define optimizer and loss functions
   loss_func = torch.nn.BCELoss()
   optim = torch.optim.Adam(model.parameters(), lr=learning_rate)
   
   # Now build ModelHandler and load pretrained LorentzNN
-  handler = mh.ModelHandler(lorentz_model, loss_func, optim, batch_size=1)
+  handler = mh.ModelHandler(lorentz_model, loss_func, optim, batch_size=200)
   handler.loadModel(model_file, True)
+  
+  # Generate the dataset in the form required by LorentzNN
+  i = 0
+  triplets = np.array((10000000,4,3))
+  for ev in np_train[:500000]:
+    for iter_i, iter in comb:
+      triplets[i] = np.take(jetv_train,iter).transpose()
+      i+=1
+  
+  input = torch.as_tensor(triplets, dtype=torch.double)
+  targets = torch.as_tensor(np.zeros(10000000), dtype=torch.double)
+  dataset = dutils.TensorDataset(input, targets)
+  dutils.DataLoader(dataset, batch_size=200, shuffle=False, drop_last=False)
   
   # Run through the data and calculate triplet probabilities
   lorentz_out = handler.model()
+  
+  # Collect the output data and organize it into events
+  trip_in = np.zeros((500000,40))
+  i = 0
+  j = 0
+  for trip in lorentz_out:
+    for match in trip:
+      trip_in[i][j] = match
+      j+=1
+      if j == 40:
+        i+=1
+        j=0
   
   # initialize triplet_tagger layer
   triplet_model = triplet_tagger()
   
   # Train the triplet_tagger layer
   max_epoch = 20
+  num_minibatch = 
   for epoch in range(max_epoch):
     epoch_loss = 0
     
     for minibatch in range(num_minibatch):
       out = model(inputMiniBatches[miniBatch])
-      loss = loss_func(out,outputMiniBatches[miniBatch])
-      loss_epoch += loss.item()
+      item_loss = loss(out,outputMiniBatches[miniBatch])
+      epoch_loss += item_loss.item()
 
       optimizer.zero_grad()
       loss.backward()
