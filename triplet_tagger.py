ad17ba968cf62580be337a00be4ce3ea28c869c4
Bobby Schiller <bobbyschiller99@gmail.com>
2020-09-30 13:20:36 -0400

    add ability to change batch size

--- triplet_tagger.py
+++ triplet_tagger.py
@@ -1,8 +1,8 @@
 ## Class to define final, fully-connected layer
 # [input] : [40,1] - softmax outputs of all 20 lorentzNN triplets
-# [output] : [21,1] - softmax output tagging correct triplet (or none)
+# [output] : [21,1] - raw logit output tagging correct triplet (or none)
 # Author: Bobby Schiller
-# Last Modified: 9 September 2020
+# Last Modified: 30 September 2020
 
 import numpy as np
 import torch
@@ -13,17 +13,23 @@ class triplet_tagger(torch.nn.Module):
   def __init__(self, **kwargs):
     super(triplet_tagger, self).__init__()
     self.lorentz_model = lNN.LorentzNN("/scratch365/rschill1/nn/p_standard1.npz",i_shape=(200,4,3),device=torch.device('cuda')).to(device=torch.device('cuda'))
-    self.layer1 = torch.nn.Linear(40,21)
-    self.activation1 = torch.nn.LeakyReLU()
-    self.layer2 = torch.nn.Linear(21,21)
+    self.layer1 = torch.nn.Linear(40,40)
+    self.activation1 = torch.nn.ReLU()
+    self.layer2 = torch.nn.Linear(40,21)
   
   def forward(self, x, **kwargs):
-    # Unpack the events from each batch and run lorentzNN
-    x = torch.flatten(x,end_dim=1)
-    x = (self.lorentz_model(x)).to(device=torch.device('cuda'))
-    
+    # LorentzNN uses batch size of 200, so shaping input batches to fit
+    N = len(x)
+    lorentz_batches = int(N/10)
+    x = torch.reshape(x,(lorentz_batches,10,20,4,3))
+    x1 = torch.zeros((lorentz_batches,200,2))
+
+    # Run triplet batches through LorentzNN
+    for i in range(lorentz_batches):
+      x1[i] = (self.lorentz_model((x[i].flatten(end_dim=1)))).to(device=torch.device('cuda'))
+
     # Reshape back to input size for triplet_tagger
-    x = torch.reshape(x,(10,40))
+    x = torch.reshape(x1,(N,40)).to(device=torch.device('cuda'),dtype=torch.double)
     x = self.layer1(x)
     x = self.activation1(x)
     x = self.layer2(x)
